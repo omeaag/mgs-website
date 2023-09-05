6fa02487c727f8c77923d99148a634e00e9febdb
Taha Ören <taha33oren@gmail.com>
2023-09-05 20:44:45 +0300

    Finish OurService &Created seperate css file

--- app/page.jsx
+++ app/page.jsx
@@ -3,13 +3,14 @@
 import React from 'react';
 import Footer from '../components/Footer';
 import OurServices from '../components/OurServices';
+import techPartners from '../Components/techPartners';
 
 const YourPage = () => {
   return (
     <div>
       {/* Your page content */}
       <OurServices />
-      <Footer />
+      <techPartners />
     </div>
   );
 };
