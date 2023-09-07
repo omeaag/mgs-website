a8d3c115a50852abe09763abefa5debbedd72621
Alp Eren <erenalpturk@icloud.com>
2023-09-07 18:34:38 +0300

    OurPortFolio part updated

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import Footer from "@/Components/Footer";
 import WhatWeDo from "@/Components/WhatWeDo";
 import OurSolution from "@/Components/OurSolution";
+import OurPortfolio from "@/Components/OurPortfolio";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({ children }) {
         <Header />
         <WhatWeDo />
         <OurSolution/>
+        <OurPortfolio/>
         {children}
         <Footer />
       </body>
