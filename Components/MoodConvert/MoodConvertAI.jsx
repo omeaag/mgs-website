c52abf73fac3dcce3cd419e71e16192c323bb00e
Alp Eren <erenalpturk@icloud.com>
2023-09-10 12:47:12 +0300

    fixed partnership&moodConvertAI input

--- Components/MoodConvert/MoodConvertAI.jsx
+++ Components/MoodConvert/MoodConvertAI.jsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import Script from 'next/script'
 import styles from "../../public/style/MoodConvertAI.css"
 import logo from "../../public/materials/moodConverAIImages/Frame.svg";
+import sendImage from "../../public/materials/moodConverAIImages/ic_baseline-send.svg";
 
 
 const MoodConvertAI = () => {
@@ -21,39 +22,44 @@ const MoodConvertAI = () => {
             <form className="AIformContainer" action="">
                 <h4>Your Personal Project Assistant ✍️</h4>
                 <div className='underForm'>
-                <div className='groupCont'>
-                    <div className='cardContainer'>
-                        <div className='textContainer'>
-                            <h5>Back End development</h5>
-                            <h6>I want to get a quotation for android development</h6>
+                    <div className='groupCont'>
+                        <div className='cardContainer'>
+                            <div className='textContainer'>
+                                <h5>Back End development</h5>
+                                <h6>I want to get a quotation for android development</h6>
+                            </div>
+                            <input className="checkbox" type="checkbox" />
                         </div>
-                        <input className="checkbox" type="checkbox" />
-                    </div>
-                    <div className='cardContainer'>
-                        <div className='textContainer'>
-                            <h5>UI/UX Design</h5>
-                            <h6>I want to get a quotation for android development</h6>
+                        <div className='cardContainer'>
+                            <div className='textContainer'>
+                                <h5>UI/UX Design</h5>
+                                <h6>I want to get a quotation for android development</h6>
+                            </div>
+                            <input className="checkbox" type="checkbox" />
                         </div>
-                        <input className="checkbox" type="checkbox" />
                     </div>
-                </div>
-                <div className='groupCont'>
-                    <div className='cardContainer'>
-                        <div className='textContainer'>
-                            <h5>Android Application</h5>
-                            <h6>I want to get a quotation for android development</h6>
+                    <div className='groupCont'>
+                        <div className='cardContainer'>
+                            <div className='textContainer'>
+                                <h5>Android Application</h5>
+                                <h6>I want to get a quotation for android development</h6>
+                            </div>
+                            <input className="checkbox" type="checkbox" />
                         </div>
-                        <input className="checkbox" type="checkbox" />
-                    </div>
-                    <div className='cardContainer'>
-                        <div className='textContainer'>
-                            <h5>Design a website</h5>
-                            <h6>I want to get a quotation for android development</h6>
+                        <div className='cardContainer'>
+                            <div className='textContainer'>
+                                <h5>Design a website</h5>
+                                <h6>I want to get a quotation for android development</h6>
+                            </div>
+                            <input className="checkbox" type="checkbox" />
                         </div>
-                        <input className="checkbox" type="checkbox" />
                     </div>
-                </div>
-                <input placeholder="Request A Proejct" type="text" id="details" />
+                    <div className='bottomCont'>
+                        <input placeholder="Request A Proejct" type="text" className="inputArea"></input>
+                        <button className="next-button">
+                            <Image className='sendButton' src={sendImage} alt="Send"  />
+                        </button>
+                    </div>
                 </div>
             </form>
         </div>
