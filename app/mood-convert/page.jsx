8dddfba4e5c28d3c3bb6219a1b4d567bcc5d541a
Okkes <okkesakkurt46@gmail.com>
2023-09-11 21:45:02 +0300

    some fixing mood-convert

--- app/mood-convert/page.jsx
+++ app/mood-convert/page.jsx
@@ -1,10 +1,18 @@
+import Feedback from "@/Components/MoodConvert/Feedback";
 import MoodConvertAI from "@/Components/MoodConvert/MoodConvertAI";
+import ReadyToTalk from "@/Components/MoodConvert/ReadyToTalk";
+import dynamic from "next/dynamic";
 
 export default function Home() {
+  const ReadyToTalk = dynamic(() => import('../../Components/MoodConvert/ReadyToTalk'), {
+      ssr: false,
+  });
 
   return (
     <div>
       <MoodConvertAI />
+      <Feedback />
+      <ReadyToTalk/>
     </div>
   );
 }
