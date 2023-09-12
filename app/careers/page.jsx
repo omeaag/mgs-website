d193d92809fe408d385eae75833841912be0fa6d
Okkes <okkesakkurt46@gmail.com>
2023-09-12 23:09:21 +0300

    css codes fixed

--- app/careers/page.jsx
+++ app/careers/page.jsx
@@ -1,9 +1,11 @@
 import CareersHeader from "@/Components/CareersHeader";
+import Hiring from "@/Components/Hiring";
 
 export default function Home() {
   return (
     <div>
       <CareersHeader />
+      <Hiring/>
     </div>
   );
 }
