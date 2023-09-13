070ce67e72a7d72ad23166d8b6224eaad929e0d2
omer can <omercansoyu@gmail.com>
2023-09-13 22:53:25 +0300

    conflicts fixed

--- app/careers/page.jsx
+++ app/careers/page.jsx
@@ -1,19 +1,13 @@
 import CareersHeader from "@/Components/CareersHeader";
-<<<<<<< HEAD
 import OurTeam from "@/Components/OurTeam/OurTeam";
-=======
 import Hiring from "@/Components/Hiring";
->>>>>>> 4f4e61ab2290757187f91b03c7accfcc594d0c67
 
 export default function Home() {
   return (
     <div>
       <CareersHeader />
-<<<<<<< HEAD
       <OurTeam />
-=======
-      <Hiring/>
->>>>>>> 4f4e61ab2290757187f91b03c7accfcc594d0c67
+      <Hiring />
     </div>
   );
 }
