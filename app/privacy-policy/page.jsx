7d742540c1b66ec35a39d403bd838c26da405b06
Taha Ören <taha33oren@gmail.com>
2023-10-13 22:53:29 +0300

    Privacy Policy

---
+++ app/privacy-policy/page.jsx
@@ -0,0 +1,12 @@
+
+import GetInTouch from "@/Components/GetInTouch";
+import PrivacyPolicy from "@/Components/Privacypolicy";
+
+export default function Home() {
+    return (
+        <div>
+            <PrivacyPolicy />
+            <GetInTouch />
+        </div>
+    );
+}
