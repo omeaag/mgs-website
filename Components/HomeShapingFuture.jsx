fbf6a9d814bcd19d69656fff515d69b193ec0163
Okkes <okkesakkurt46@gmail.com>
2023-10-19 14:51:34 +0300

    added animation to home-header

--- Components/HomeShapingFuture.jsx
+++ Components/HomeShapingFuture.jsx
@@ -1,52 +1,67 @@
-import { useEffect, useState } from "react";
-import "../public/style/HomeShaping.css";
+import {useEffect, useState} from 'react';
+import '../public/style/HomeShaping.css';
+import styled from 'styled-components';
+import {useTransition, animated} from 'react-spring';
+
+const SlotMachineWrapper = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const WordWrapper = styled(animated.div)`
+    margin: 0 10px;
+`;
+const words = ['Blockchain.', 'Development.', 'Technology.', 'Design.'];
 
 export default function HomeShapingFuture() {
-  let texts = [
-    "Blockchain.",
-    "Development",
-    "Technology",
-    "Design",
-  ];
-  const [index, setIndex] = useState(0);
-  const [currentText, setCurrentText] = useState(texts[index]);
+ const [index, setIndex] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setIndex((prevIndex) => (prevIndex + 1) % texts.length);
-    }, 3000);
+ useEffect(() => {
+     const interval = setInterval(() => {
+         setIndex((index + 1) % words.length);
+     }, 3000);
 
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
+     return () => {
+         clearInterval(interval);
+     };
+ }, [index]);
 
-  useEffect(() => {
-    setCurrentText(texts[index]);
-  }, [index]);
+  const transitions = useTransition(words[index], {
+      from: {opacity: 0, transform: 'translate3d(0, -50px, 0)'},
+      enter: {opacity: 1, transform: 'translate3d(0, 0, 0)'},
 
-  return (
-    <div className="HomeShapingContainer">
-      <div className="HomeShapingHeaders">
-        <div className="firstRow">
-          <p className="text-shaping">Think</p>
-          <p className="text-creativity"> {currentText}</p>
+  });
+
+    return (
+        <div className='HomeShapingContainer'>
+            <div className='HomeShapingHeaders'>
+                <div className='firstRow'>
+                    <p className='text-shaping'>Think</p>
+                    <p className='text-creativity'>
+                        <SlotMachineWrapper>
+                            {transitions((style, word) => (
+                                <WordWrapper style={style}>{word}</WordWrapper>
+                            ))}
+                        </SlotMachineWrapper>
+                    </p>
+                </div>
+                <p className='text-shaping'>Think Mood</p>
+            </div>
+
+            <div className='HomeShapingBottomLine'></div>
+
+            <div className='HomeShapingParagraph'>
+                <p className='at-mood-global'>
+                    At Mood Global Services, we're committed to crafting and setting up digital
+                    infrastructures that enhance valuable network communication and stimulate
+                    business progression.
+                </p>
+            </div>
+
+            <div className='HomeShapingbuttons'>
+                <button className='homeshapping-btn black'>Get Started</button>
+                <button className='homeshapping-btn white'>Learn More</button>
+            </div>
         </div>
-        <p className="text-shaping">Think Mood</p>
-      </div>
-
-      <div className="HomeShapingBottomLine"></div>
-
-      <div className="HomeShapingParagraph">
-        <p className="at-mood-global">
-          At Mood Global Services, we're committed to crafting and setting up digital infrastructures that enhance valuable network communication and stimulate business progression.
-        </p>
-      </div>
-
-      <div className="HomeShapingbuttons">
-        <button className="homeshapping-btn black">Get Started</button>
-        <button className="homeshapping-btn white">Learn More</button>
-      </div>
-    </div>
-  );
+    );
 }
