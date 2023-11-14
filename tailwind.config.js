3697eda2b38c22f35d659fb8d722af3706f96a55
Ökkeş Akkurt <okkesakkurt46@gmail.com>
2023-11-14 20:00:19 +0300

    hello world tailwind working

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./components/**/*.{html,jsx}'],
+	content: ['./components/**/*.{html,jsx,js}'],
 	theme: {
 		extend: {},
 	},
